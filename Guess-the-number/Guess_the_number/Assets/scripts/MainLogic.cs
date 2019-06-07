26bdb05b09b7836805fbaecdc2335bb18bc237d7
MI shanto <shanto4045@gmail.com>
2019-06-07 23:54:12 +0600

    game file with build apk v-1.0

---
+++ Guess-the-number/Guess_the_number/Assets/scripts/MainLogic.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainLogic : MonoBehaviour
+{
+    public Text txt;
+    int min = 1;
+    int max = 1000+1;
+    int res;
+    void Start()
+    {
+   
+        res = (min + max) / 2;
+    }
+
+    public void submit()
+    {
+        txt.text = res.ToString();
+    }
+    public void higher()
+    {
+        min = res;
+        max = max;
+        res = (min + max) / 2;
+        txt.text = res.ToString();
+    }
+    public void lower()
+    {
+        min = min;
+        max = res;
+        res = (min+max)/ 2;
+        txt.text = res.ToString();
+    }
+}
